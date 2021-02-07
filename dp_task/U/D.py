f7e9167a7ccdce00687ee39279d5116ef4c4c8f8
chenjx-zju <3150101223@zju.edu.cn>
2021-02-07 00:04:24 -0600

    TUVWXYZ init

---
+++ dp_task/U/D.py
@@ -0,0 +1,22 @@
+import collections
+
+def readLine():
+	return list(map(int, input().strip().split()))
+
+def readInt():
+	return int(input())
+
+def readString():
+	return input()
+
+def tcase():
+	t = readInt()
+	for _ in range(t):
+		solve()
+
+def solve():
+	
+
+
+# tcase()
+solve()
