5bd1048a9070d64701fd26bac11f56ef0f0b23c3
chenjx-zju <3150101223@zju.edu.cn>
2021-02-16 13:15:48 -0600

    X ac

--- dp_task/X/D.py
+++ dp_task/X/D.py
@@ -15,6 +15,21 @@ def tcase():
 		solve()
 
 def solve():
+	n = readInt()
+	arr = [readLine() for _ in range(n)]
+	arr.sort(key=lambda x: (x[0] + x[1], x[0]))
+
+	dp = [[0 for _ in range(20050)] for _ in range(n+1)]
+
+	for i in range(1, n+1):
+		cw, cs, cv = arr[i-1]
+		for w in range(20050):
+			dp[i][w] = dp[i-1][w]
+			if w >= cw and w <= cs + cw:
+				dp[i][w] = max(dp[i][w], dp[i-1][w - cw] + cv)
+
+	print(max(dp[-1]))
+
 	
 
 
